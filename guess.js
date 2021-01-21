43c84c04aa202c2f9c2e96f73a6bcfe95c88bf2f
Mark Brown <mark.m3wb@gmail.com>
2021-01-21 06:53:52 -0600

    Initial Build Commit

---
+++ guess.js
@@ -0,0 +1,23 @@
+const randomNumber = Math.floor((Math.random() * 100) / 10 ) + 1
+console.log("Random Number ", randomNumber);
+
+function checkGuess() {
+    let myGuess = guess.value
+    if (myGuess == randomNumber) {
+        feedback.textContent = "You got it! Nice job! Reresh the page to play again.";
+    } 
+
+    else if (myGuess > randomNumber && myGuess <= 10) {
+        feedback.textContent = "Your guess was " + myGuess + ", that's too high. Try again.";
+     }
+    
+    else if (myGuess < randomNumber && myGuess > 0) {
+        feedback.textContent = "Your guess was " + myGuess + ", that's too low. Try again.";
+    }
+
+    else if (myGuess < 0 || myGuess > 10) {
+        feedback.textContent = "Hey, I said between 1 and 10. You're way off. Try again.";
+    }
+}
+
+submitGuess.addEventListener('click', checkGuess);
